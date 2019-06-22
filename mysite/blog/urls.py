bd7f3635c09d1a056d97a078acead3341b931457
jisanglee <newspatoy1@gmail.com>
2019-06-22 16:56:02 +0900

    add post edit

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #all post
     path('', views.post_list, name='post_list'),
+    #one post
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #new post
+    path('post/new', views.post_new, name='post_new'),
+    #edit post
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
