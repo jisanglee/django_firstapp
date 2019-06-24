273b1bf9ed0981ae768a89b781cee8e9ff3a4a84
jisanglee <newspatoy1@gmail.com>
2019-06-24 18:30:42 +0900

    comment

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post) #Post 모델 등록.
-
+admin.site.register(Comment) #Comment 모델 등록.
