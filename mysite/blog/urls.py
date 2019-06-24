94fa2f6399f3f221d34e007c6f8889e0a0e05974
jisanglee <newspatoy1@gmail.com>
2019-06-24 09:31:27 +0900

    draft page, publish, delete post

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -10,4 +11,8 @@ urlpatterns = [
     path('post/new', views.post_new, name='post_new'),
     #edit post
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+
 ]
